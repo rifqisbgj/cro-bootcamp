89eb1605139c694e9f75159758b44b94e048d666
Rifqi Subagja <rifqi.subagja4@gmail.com>
2023-01-07 12:27:20 +0700

    tes

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -44,7 +44,7 @@ class CheckoutController extends Controller
         $data['user_id'] = Auth::id();
         $data['camp_id'] = $camp->id;
 
-        // update occupation user
+        // update occupation use
         $user = Auth::user();
         $user->occupation = $data['occupation'];
         $user->save();
